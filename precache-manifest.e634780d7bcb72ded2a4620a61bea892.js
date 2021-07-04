6065c1840d072d9739276a1bb9c1272e6b81786c
James Fulford <james.fulford@outlook.com>
2021-07-04 13:41:37 -0400

    Updates

---
+++ precache-manifest.e634780d7bcb72ded2a4620a61bea892.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e8d703b2f5c662d679bdd8a0bf40524",
+    "url": "/weather-candlestick/index.html"
+  },
+  {
+    "revision": "58f194eea0293d424c30",
+    "url": "/weather-candlestick/static/css/main.6abb1746.chunk.css"
+  },
+  {
+    "revision": "162953fca9d621a7eaa8",
+    "url": "/weather-candlestick/static/js/2.8db376ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/weather-candlestick/static/js/2.8db376ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f194eea0293d424c30",
+    "url": "/weather-candlestick/static/js/main.795ef4b2.chunk.js"
+  },
+  {
+    "revision": "238eb09632c42e0d00e4",
+    "url": "/weather-candlestick/static/js/runtime-main.83ae87e4.js"
+  }
+]);
