d181b2404451ac439e9206d6415a089594f91ca8
James Fulford <james.fulford@outlook.com>
2021-07-04 13:35:48 -0400

    feat(*): Add candlestick chart

--- src/components/Conditions/Conditions.js
+++ src/components/Conditions/Conditions.js
@@ -4,8 +4,10 @@ import {
     Small,
     Loader
 } from './Conditions.module.css'
+import Chart from "react-google-charts";
 
 const conditions = (props) => {
+    console.log(props);
     return (
         <div className={Wrapper}>
 
@@ -14,11 +16,39 @@ const conditions = (props) => {
             {props.loading && <div className={Loader} />}
 
 
-            {props.responseObj.cod === 200 ?
-                <div>
-                    <p><strong>{props.responseObj.name}</strong></p>
-                    <p>It is currently {Math.round(props.responseObj.main.temp)} degrees out with {props.responseObj.weather[0].description}.</p>
-                </div>
+            {Number(props.responseObj.cod) === 200 ?
+                <>
+
+                    <Chart
+                        width={'100%'}
+                        height={350}
+                        chartType="CandlestickChart"
+                        loader={<div>Loading Chart</div>}
+                        data={[
+                            ['day', 'a', 'b', 'c', 'd'],
+                            ...props.responseObj.list.map(l => [
+                                new Date(l.dt * 1000).toLocaleDateString(),
+                                l.temp.min,
+                                l.temp.morn,
+                                l.temp.night,
+                                l.temp.max,
+                            ])
+                        ]}
+                        options={{
+                            legend: 'none',
+                            bar: { groupWidth: '100%' }, // Remove space between bars.
+                            candlestick: {
+                            fallingColor: { strokeWidth: 0, fill: '#a52714' }, // red
+                            risingColor: { strokeWidth: 0, fill: '#0f9d58' }, // green
+                            },
+                        }}
+                        rootProps={{ 'data-testid': 'candlestick-chart' }}
+                    />
+                
+                    {props.responseObj.list.map(l => {
+                        return <code>{new Date(l.dt * 1000).toLocaleDateString()} {l.temp.morn} {l.temp.min} {l.temp.max} {l.temp.eve}</code>
+                    })}
+                </>
             : null
             }
         </div>
--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,69 +1,53 @@
 import React, { useState } from 'react';
 import Conditions from '../Conditions/Conditions';
 import {
-    textInput,
     Radio,
     Button
 } from './Forecast.module.css';
 
 const Forecast = () => {
-
-    let [city, setCity] = useState('');
+    const [lat, lon] = [42.99, -71.46]
     let [unit, setUnit] = useState('imperial');
     let [responseObj, setResponseObj] = useState({});
     let [error, setError] = useState(false);
     let [loading, setLoading] = useState(false);
 
-function getForecast(e) {
-    e.preventDefault();
+    function getForecast(e) {
+        e.preventDefault();
 
-    if (city.length === 0) {
-        return setError(true);
+        // Clear state in preparation for new data
+        setError(false);
+        setResponseObj({});
+        
+        setLoading(true);
+
+        fetch(`https://community-open-weather-map.p.rapidapi.com/forecast/daily?lat=${lat}&lon=${lon}&cnt=16&units=${unit}`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (Number(response.cod) !== 200) {
+                throw new Error()
+            }
+
+            setResponseObj(response);
+            setLoading(false);
+        })
+        .catch(err => {
+            setError(true);
+            setLoading(false);
+            console.log(err.message);
+        });
     }
-
-    // Clear state in preparation for new data
-    setError(false);
-    setResponseObj({});
-    
-    setLoading(true);
-    
-    const uriEncodedCity = encodeURIComponent(city);
-
-    fetch(`https://community-open-weather-map.p.rapidapi.com/weather?units=${unit}&q=${uriEncodedCity}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-            "x-rapidapi-key": process.env.REACT_APP_API_KEY
-        }
-    })
-    .then(response => response.json())
-    .then(response => {
-        if (response.cod !== 200) {
-            throw new Error()
-        }
-
-        setResponseObj(response);
-        setLoading(false);
-    })
-    .catch(err => {
-        setError(true);
-        setLoading(false);
-        console.log(err.message);
-    });
-}
 
     return (
         <div>
             <h2>Find Current Weather Conditions</h2>
             <form onSubmit={getForecast}>
-                <input
-                    type="text"
-                    placeholder="Enter City"
-                    maxLength="50"
-                    className={textInput}
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                    />
                 <label className={Radio}>
                     <input
                         type="radio"
